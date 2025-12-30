cdbf82ba64724eb864f47d57c64dcde239fe3004
muzam <muzamelsas@gmail.com>
2025-12-30 14:10:22 +0800

    aku dh edit saja2

--- src/propertySystem.java
+++ src/propertySystem.java
@@ -40,6 +40,7 @@ public class propertySystem {
                     running = false;
                     break;
                 default:
+                    // babi
                     System.out.println("Invalid choice. Please try again.");
             }
         }
