992664115fd059d234a810abe9693e0ea18cb19d
muzam <muzamelsas@gmail.com>
2026-01-02 12:30:25 +0800

    tukar nama

--- src/propertyManagerMachine.java
+++ src/propertyManagerMachine.java
@@ -2,27 +2,45 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+// File: PropertyManager.java
+// Make sure this matches your package name (e.g., package propertymanagementsystem;)
 
-/**
- *
- * @author User
- */
-public class propertyManagerMachine {
-        public class propertyList {
-        private int[] property = new int[100]; 
-        private int count = 0;
-        public void addProperties(int[] Properties) {
-            System.out.println("[System] Adding property...");
-            if(count < Properties.length) {
-                Properties[count] = count + 1; 
-                count++;
-                System.out.println("[System] Property added successfully. Total properties: " + count);
-            } else {
-                System.out.println("[System] Property list is full. Cannot add more properties.");
-            }
+public class PropertyManagerMachine {
+    // 1. Storage: We store 'Property' objects, not 'int'
+    // NOTE: This will show an ERROR until you create the Property.java file below.
+    private Property[] properties = new Property[100]; 
+    private int count = 0;
+
+    // 2. Add Method
+    public void addProperty(Property p) {
+        if (count < properties.length) {
+            properties[count] = p; // Store the object
+            count++; // Increment counter
+            System.out.println("[System] Property added successfully.");
+        } else {
+            System.out.println("[Error] Storage is full.");
         }
-        
-        
     }
 
+    // 3. Display Method
+    public void viewProperties() {
+        if (count == 0) {
+            System.out.println("No properties found.");
+            return;
+        }
+        System.out.println("\n--- Property List ---");
+        for (int i = 0; i < count; i++) {
+            // We call the print method inside the Property object
+            properties[i].printDetails(); 
+        }
+    }
+    
+    // 4. Helper for Hadif
+    public Property[] getAllProperties() {
+        return properties;
+    }
+    
+    public int getCount() {
+        return count;
+    }
 }
